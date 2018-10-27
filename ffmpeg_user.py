e81e235b4e44ddd42d2616888f548bcebc252b5c
Богдан <44440399+BohdanOpyr@users.noreply.github.com>
2018-10-27 12:25:46 +0300

    Update ffmpeg_user.py

--- ffmpeg_user.py
+++ ffmpeg_user.py
@@ -1,2 +1,12 @@
-def join(video1, video2):
-    pass
+import os
+class Video:
+    def __init__(self,path,name=None):
+        self.path = path
+        if name == None:
+            self.name = self.path.split(‘/’)[-1]
+        else:
+            self.name = name
+        os.system(‘mkdir /tmp/%s’%self.name)
+        os.system(‘ffmpeg -i %s /tmp/%s/frame%%d.gif /tmp/%s/audio.aac’%(self.path,self.name,self.name))
+
+
