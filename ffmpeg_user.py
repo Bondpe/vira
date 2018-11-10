07783085cdb4262b0ac2adac6ccb21905a9553e8
Богдан <44440399+BohdanOpyr@users.noreply.github.com>
2018-11-10 12:05:05 +0200

    making command 'video1 += video2' supported

--- ffmpeg_user.py
+++ ffmpeg_user.py
@@ -8,7 +8,25 @@ class Video:
             amount += 1
         else:
             self.name = name
-        os.system(‘mkdir /tmp/%s’%self.name)
-        os.system(‘ffmpeg -i %s /tmp/%s/frame%%d.gif /tmp/%s/audio.aac’%(self.path,self.name,self.name))
+        os.system(‘mkdir /tmp/vira/%s’%self.name)
+        os.system(‘ffmpeg -i %s /tmp/vira/%s/frame%%d.png /tmp/%s/audio.aac’%(self.path,self.name,self.name))
+        num = 0
+        while True:
+            num += 1
+            try:
+                open('/tmp/vira/%s/frame%d.png'%num)
+            except:
+                self.len = num
+                break
+    def _iadd_(self,video):
+        for num in range(video.len):
+            frame = open('/tmp/vira/%s/frame%d.png'%(video.name,num),'rb').read()
+            self.len += 1
+            file = open('/tmp/vira/%s/frame%d.png'%(self.name, self.len), 'wb')
+            file.write(frame)
+            file.close()
+            
+
+
 
 
