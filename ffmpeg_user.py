90fffe5f311110a0a8f73b872d6e14d1ad63a8fc
Богдан <44440399+BohdanOpyr@users.noreply.github.com>
2018-11-10 11:41:18 +0200

    Update ffmpeg_user.py

--- ffmpeg_user.py
+++ ffmpeg_user.py
@@ -1,9 +1,11 @@
 import os
+amount = 0
 class Video:
     def __init__(self,path,name=None):
         self.path = path
         if name == None:
-            self.name = self.path.split(‘/’)[-1]
+            self.name = str(amount)
+            amount += 1
         else:
             self.name = name
         os.system(‘mkdir /tmp/%s’%self.name)
