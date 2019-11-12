8da8ff7d51e06a16e9d47e2c8c90ee50d951baa3
Богдан <44440399+BohdanOpyr@users.noreply.github.com>
2019-11-12 08:37:56 +0200

    Add files via upload

--- plugins/imageeffects/__init__.py
+++ plugins/imageeffects/__init__.py
@@ -1,1 +1,17 @@
+import numpy as np
+from PIL import Image
+import structure
+import input as inputs
 
+class Posterize(structure.Basic):
+    category = 'Image effects'
+    Tname = 'limit palette'
+    argnames = {'levels':None, 'child':None}
+    argtype = {'levels':inputs.Integer, 'child':'c'}
+    def get(self, time, b):
+        a = self.args['child'].get(time, b)
+        image = Image.fromarray(a)
+        result = image.convert('P', palette=Image.ADAPTIVE, colors=self.args['levels'].num).convert('RGB')
+        a = np.asarray(result)
+        return a
+data = {'structure_avaliable':[Posterize]}
