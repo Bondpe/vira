23c07593c2c37bef288cecff2899e298bf876675
Богдан <44440399+BohdanOpyr@users.noreply.github.com>
2019-11-14 16:25:31 +0200

    Add files via upload

--- plugins/imagemagick/__init__.py
+++ plugins/imagemagick/__init__.py
@@ -4,6 +4,16 @@ import input as inputs
 import numpy as np
 from PIL import Image
 
+class RP:
+    def __init__(self):
+        self.tmp='/tmp/vira/imagemagick'
+        os.mkdir(self.tmp)
+        self.id = 0
+    def __call__(self):
+        self.id+=1
+        return self.tmp+'/'+str(self.id)
+rp = RP()
+
 class Charcoal(structure.Basic):
     category = 'Imagemagick'
     Tname = 'charcoal'
@@ -11,10 +21,11 @@ class Charcoal(structure.Basic):
     argtype = {'radius':inputs.Integer, 'child':'c'}
     def get(self, time, b):
         a = self.args['child'].get(time, b)
-        name = constants.get_temp_path()+'.png'
+        name = rp()+'.png'
+        out = rp()+'_.png'
         Image.fromarray(a).save(name)
-        os.system('convert %s -charcoal %d %s'%(name,self.args['radius'].num,name))
-        return np.asarray(Image.open(name).convert('RGB'))
+        os.system('convert %s -charcoal %d %s'%(name,self.args['radius'].num,out))
+        return np.asarray(Image.open(out).convert('RGB'))
 class Implode(structure.Basic):
     category = 'Imagemagick'
     Tname = 'implode'
@@ -22,10 +33,11 @@ class Implode(structure.Basic):
     argtype = {'radius':inputs.Integer, 'child':'c'}
     def get(self, time, b):
         a = self.args['child'].get(time, b)
-        name = constants.get_temp_path()+'.png'
+        name = rp()+'.png'
+        out = rp()+'_.png'
         Image.fromarray(a).save(name)
-        os.system('convert %s -implode %d %s'%(name,self.args['radius'].num,name))
-        return np.asarray(Image.open(name).convert('RGB'))
+        os.system('convert %s -implode %d %s'%(name,self.args['radius'].num,out))
+        return np.asarray(Image.open(out).convert('RGB'))
 class Rotate(structure.Basic):
     category = 'Imagemagick'
     Tname = 'rotate'
@@ -33,10 +45,11 @@ class Rotate(structure.Basic):
     argtype = {'degrees':inputs.Integer, 'child':'c'}
     def get(self, time, b):
         a = self.args['child'].get(time, b)
-        name = constants.get_temp_path()+'.png'
+        name = rp()+'.png'
+        out = rp()+'_.png'
         Image.fromarray(a).save(name)
-        os.system('convert %s -rotate %d %s'%(name,self.args['degrees'].num,name))
-        return np.asarray(Image.open(name).convert('RGB'))
+        os.system('convert %s -rotate %d %s'%(name,self.args['degrees'].num,out))
+        return np.asarray(Image.open(out).convert('RGB'))
 class Swirl(structure.Basic):
     category = 'Imagemagick'
     Tname = 'swirl'
@@ -44,10 +57,11 @@ class Swirl(structure.Basic):
     argtype = {'degrees':inputs.Integer, 'child':'c'}
     def get(self, time, b):
         a = self.args['child'].get(time, b)
-        name = constants.get_temp_path()+'.png'
+        name = rp()+'.png'
+        out = rp()+'_.png'
         Image.fromarray(a).save(name)
-        os.system('convert %s -swirl %d %s'%(name,self.args['degrees'].num,name))
-        return np.asarray(Image.open(name).convert('RGB'))
+        os.system('convert %s -swirl %d %s'%(name,self.args['degrees'].num,out))
+        return np.asarray(Image.open(out).convert('RGB'))
 class Posterize(structure.Basic):
     category = 'Imagemagick'
     Tname = 'posterize'
@@ -55,8 +69,9 @@ class Posterize(structure.Basic):
     argtype = {'levels':inputs.Integer, 'child':'c'}
     def get(self, time, b):
         a = self.args['child'].get(time, b)
-        name = constants.get_temp_path()+'.png'
+        name = rp()+'.png'
+        out = rp()+'_.png'
         Image.fromarray(a).save(name)
-        os.system('convert %s -posterize %d %s'%(name,self.args['levels'].num,name))
-        return np.asarray(Image.open(name).convert('RGB'))
+        os.system('convert %s -posterize %d %s'%(name,self.args['levels'].num,out))
+        return np.asarray(Image.open(out).convert('RGB'))
 data = {'structure_avaliable':[Charcoal, Implode, Rotate, Swirl, Posterize]}
